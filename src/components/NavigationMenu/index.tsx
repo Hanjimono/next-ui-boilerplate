6c015737207d1c7d53e15a2002e0d9fb2d942886
Anton Myasnikov <hanjimonor@gmail.com>
2025-07-23 15:06:22 +0400

    Main: make menu more simp;e

--- src/components/NavigationMenu/index.tsx
+++ src/components/NavigationMenu/index.tsx
@@ -1,14 +1,7 @@
 "use client"
-// system
-import { useState } from "react"
-import clsx from "clsx"
 // ui
 import Menu from "@/ui/Navigation/Menu"
 import Beam from "@/ui/Layout/Beam"
-// styles and types
-import Button from "@/ui/Actions/Button"
-import Title from "@/ui/Presentation/Title"
-import Logo from "../Logo"
 
 const MENU = [
   {
@@ -37,31 +30,10 @@ const MENU = [
 
 // component with main navigation menu on the left side of every page
 function NavigationMenu() {
-  const [openMenu, setOpenMenu] = useState(false)
   return (
-    <>
-      <div>
-        <Button
-          onClick={() => setOpenMenu(true)}
-          icon="menu"
-          text
-          iconSize={30}
-        />
-        <Logo />
-      </div>
-      <div>
-        <Button
-          onClick={() => setOpenMenu(false)}
-          icon="close"
-          text
-          iconSize={22}
-        />
-        <Logo />
-        <Beam withoutWrap whole>
-          <Menu className="w-full" items={MENU} />
-        </Beam>
-      </div>
-    </>
+    <Beam withoutWrap className="w-48">
+      <Menu className="w-full" items={MENU} />
+    </Beam>
   )
 }
 export default NavigationMenu
